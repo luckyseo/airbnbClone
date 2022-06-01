fb2ba7bb5e6057bb04bf86f56bdb3f5e2e3f13d8
luckyseo <ityun777@gmail.com>
2022-06-01 17:01:31 +0900

    la,cur, superhost, birthdate +

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from locale import currency
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -14,6 +15,26 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
-    bio = models.TextField(default="")  # or null = True
+    LANGUAGE_ENG = "english"
+    LANGUAGE_KOR = "korean"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENG, "en"), (LANGUAGE_KOR, "kr"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)  # or null = True
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
